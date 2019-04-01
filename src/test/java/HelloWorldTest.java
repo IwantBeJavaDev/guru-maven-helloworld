b2623fb649b396dd5e9edfb8c41d5fc2f02a8e11
Michal <m.baraniak@streamsoft.pl>
2019-04-01 13:37:01 +0200

    Proces lifecyce and

---
+++ src/test/java/HelloWorldTest.java
@@ -0,0 +1,11 @@
+public class HelloWorldTest {
+
+
+    public void testWorld(){
+        System.out.println("TEST - Hello");
+    }
+
+    public void test1_World(){
+        System.out.println("TEST - !!!!!");
+    }
+}
